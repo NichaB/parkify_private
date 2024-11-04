8596772e3fc9538e74c4a78a3db33f458166fdbc
Chayada9871 <chayada9871@gmail.com>
2024-11-04 21:04:03 +0700

    try push 2

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,18 +1,17 @@
 // components/Header.js
-export default function Header() {
-    return (
-      <div className="flex items-center justify-between mb-6">
-        <div>
-          <p className="text-sm text-gray-500">Hello, Dr. Pakpong</p>
-          <h1 className="text-2xl font-bold">Made you easily</h1>
-          <h1 className="text-2xl font-bold text-black">Parking</h1>
-        </div>
-        <img
-          src="/images/Profile.jpeg" // Replace with actual path
-          alt="Profile"
-          className="w-10 h-10 rounded-full"
-        />
+export default function Header({ userName }) {
+  return (
+    <div className="flex items-center justify-between mb-6">
+      <div>
+        <p className="text-sm text-gray-500">Hello, {userName || "Guest"}</p>
+        <h1 className="text-2xl font-bold">Made you easily</h1>
+        <h1 className="text-2xl font-bold text-black">Parking</h1>
       </div>
-    );
-  }
-  
+      <img
+        src="/images/Profile.jpeg" // Replace with actual path to your profile image
+        alt="Profile"
+        className="w-10 h-10 rounded-full"
+      />
+    </div>
+  );
+}
