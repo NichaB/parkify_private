1f948de5da27210af5fc8dd8cb677e30cefe6277
Nicha <cahnamatta14@gmail.com>
2024-11-22 02:58:43 +0700

    add land

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ['klxuzcwcvhulmdckttju.supabase.co'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/landing',
+        permanent: true, // Use true for 301 redirects, false for 302 redirects
+      },
+    ];
+  },
 };
 
 export default nextConfig;
